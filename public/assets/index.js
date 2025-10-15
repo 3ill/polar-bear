7195dab36212a0dd3c84d9dbd3658222229debf9
3illBaby <thrillanon@outlook.com>
2025-10-15 11:03:40 +0100

    chore(assets): removed and vehicle images

---
+++ public/assets/index.js
@@ -0,0 +1,27 @@
+import JS201 from "./JS2/JS2-EX-001.jpg";
+import JS202 from "./JS2/JS2-EX-002.jpg";
+import JS205 from "./JS2/JS2-EX-005.jpg";
+import JS206 from "./JS2/JS2-EX-006.jpg";
+import JS207 from "./JS2/JS2-EX-007.jpg";
+import JS208 from "./JS2/JS2-EX-008.jpg";
+import JS209 from "./JS2/JS2-EX-009.jpg";
+import JS210 from "./JS2/JS2-EX-010.jpg";
+import JS211 from "./JS2/JS2-EX-011.jpg";
+import JS212 from "./JS2/JS2-EX-012.jpg";
+import JS213 from "./JS2/JS2-EX-013.jpg";
+import JS214 from "./JS2/JS2-EX-014.jpg";
+
+export {
+  JS201,
+  JS202,
+  JS205,
+  JS206,
+  JS207,
+  JS208,
+  JS209,
+  JS210,
+  JS211,
+  JS212,
+  JS213,
+  JS214,
+};
