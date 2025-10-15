60ce789b417856c9e01bc9933812c7ffb5aa1198
3illBaby <thrillanon@outlook.com>
2025-10-15 14:59:40 +0100

    chore(index.js): exported assets

--- public/assets/index.js
+++ public/assets/index.js
@@ -21,6 +21,18 @@ import JS307 from "./JS3/JS3-EX-007.jpg";
 import JS308 from "./JS3/JS3-EX-008.jpg";
 import JS309 from "./JS3/JS3-EX-009.jpg";
 
+// JS4 Imports
+import JS401 from "./JS4/JS4-EX-001.jpg";
+import JS402 from "./JS4/JS4-EX-002.jpg";
+import JS403 from "./JS4/JS4-EX-003.jpg";
+import JS406 from "./JS4/JS4-EX-006.jpg";
+import JS407 from "./JS4/JS4-EX-007.jpg";
+import JS408 from "./JS4/JS4-EX-008.png";
+import JS409 from "./JS4/JS4-EX-009.jpg";
+import JS410 from "./JS4/JS4-EX-010.jpg";
+import JS412 from "./JS4/JS4-EX-012.jpg";
+import JS413 from "./JS4/JS4-EX-013.png";
+
 export {
   JS201,
   JS202,
@@ -41,4 +53,14 @@ export {
   JS307,
   JS308,
   JS309,
+  JS401,
+  JS402,
+  JS403,
+  JS406,
+  JS407,
+  JS408,
+  JS409,
+  JS410,
+  JS412,
+  JS413,
 };
