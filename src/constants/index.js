74f0cbb245c64a78efd65e97185d54b7b66df438
3illBaby <thrillanon@outlook.com>
2025-10-16 11:00:58 +0100

    added images to canvas object

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,6 +33,7 @@ import BlackSUV from "../components/black-suv";
 import BlackTruck from "../components/black-truck";
 import BlueTruck from "../components/blue-truck";
 import GreyTruck from "../components/grey-truck";
+import Js2Car from "../components/js8-car";
 import RedSUV from "../components/red-suv";
 import WhiteSUV from "../components/white-suv";
 
@@ -69,7 +70,7 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
           ? [-11, -7, -10]
           : [-11, -10, -10],
 
-    carScale: isSmall ? 9 : isMobile ? 12 : isTablet ? 12 : 17,
+    carScale: isSmall ? 9 : isMobile ? 12 : isTablet ? 12 : 18,
     carPosition: isSmall ? [0, -2, 0] : [0, -1.5, 0],
   };
 };
@@ -99,6 +100,48 @@ export const Models = [
       { color: "bg-black", component: BlackTruck },
       { color: "bg-cyan-800", component: BlueTruck },
       { color: "bg-neutral-600", component: GreyTruck },
+    ],
+  },
+  {
+    id: "JS2-SUV",
+    name: "JS2",
+    component: null,
+    defaultScale: 200,
+    defaultPosition: [0, -2, 0],
+    defaultRotation: [0, Math.PI / 2, 0],
+    colors: [
+      { color: "bg-white", image: "/assets/JS2/JS2-EX-001.jpg" },
+      { color: "bg-black", image: "/assets/JS2/JS2-EX-002.jpg" },
+      { color: "bg-[#3E2723]", image: "/assets/JS2/JS2-EX-005.jpg" },
+      { color: "bg-orange-500", image: "/assets/JS2/JS2-EX-009.jpg" },
+    ],
+  },
+  {
+    id: "JS3-SUV",
+    name: "JS3",
+    component: null,
+    defaultScale: 200,
+    defaultPosition: [0, -2, 0],
+    defaultRotation: [0, Math.PI / 2, 0],
+    colors: [
+      { color: "bg-white", image: "/assets/JS3/JS3-EX-001.jpg" },
+      { color: "bg-neutral-600", image: "/assets/JS3/JS3-EX-004.jpg" },
+      { color: "bg-[#3E2723]", image: "/assets/JS3/JS3-EX-005.jpg" },
+      { color: "bg-red-500", image: "/assets/JS3/JS3-EX-006.jpg" },
+    ],
+  },
+  {
+    id: "JS4-SUV",
+    name: "JS4",
+    component: null,
+    defaultScale: 200,
+    defaultPosition: [0, -2, 0],
+    defaultRotation: [0, Math.PI / 2, 0],
+    colors: [
+      { color: "bg-white", image: "/assets/JS4/JS4-EX-001.jpg" },
+      { color: "bg-black", image: "/assets/JS4/JS4-EX-002.jpg" },
+      { color: "bg-red-500", image: "/assets/JS4/JS4-EX-003.jpg" },
+      { color: "bg-[#3E2723]", image: "/assets/JS4/JS4-EX-006.jpg" },
     ],
   },
 ];
