f149d5776736556e45f66b91aff35621f837be2b
3illBaby <thrillanon@outlook.com>
2025-10-17 01:15:39 +0100

    preload assets

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,46 @@ const App = () => {
       "/assets/JS2/JS2-EX-002.jpg",
       "/assets/JS2/JS2-EX-005.jpg",
       "/assets/JS2/JS2-EX-009.jpg",
+      // JS3
       "/assets/JS3/JS3-EX-001.jpg",
       "/assets/JS3/JS3-EX-004.jpg",
       "/assets/JS3/JS3-EX-005.jpg",
       "/assets/JS3/JS3-EX-006.jpg",
+      // JS4
       "/assets/JS4/JS4-EX-001.jpg",
       "/assets/JS4/JS4-EX-002.jpg",
       "/assets/JS4/JS4-EX-003.jpg",
       "/assets/JS4/JS4-EX-006.jpg",
+
+      // JS6
+      "/assets/JS6/EXTERIOR/JS6-EX-001.png",
+      "/assets/JS6/EXTERIOR/JS6-EX-002.png",
+      "/assets/JS6/EXTERIOR/JS6-EX-003.png",
+      "/assets/JS6/EXTERIOR/JS6-EX-004.png",
+      "/assets/JS6/INTERIOR/JS6-IN-001.jpg",
+      "/assets/JS6/INTERIOR/JS6-IN-002.jpg",
+      "/assets/JS6/INTERIOR/JS6-IN-003.jpg",
+      "/assets/JS6/INTERIOR/JS6-IN-004.jpg",
+
+      // JS7
+      "/assets/JS7/EXTERIOR/JS7-EX-001.jpg",
+      "/assets/JS7/EXTERIOR/JS7-EX-002.jpg",
+      "/assets/JS7/EXTERIOR/JS7-EX-003.jpg",
+      "/assets/JS7/EXTERIOR/JS7-EX-004.png",
+      "/assets/JS7/INTERIOR/JS7-IN-001.jpg",
+      "/assets/JS7/INTERIOR/JS7-IN-002.jpg",
+      "/assets/JS7/INTERIOR/JS7-IN-003.jpg",
+      "/assets/JS7/INTERIOR/JS7-IN-004.jpg",
+
+      // T8 PRO
+      "/assets/T8/EXTERIOR/T8-R-EX-001.jpg",
+      "/assets/T8/EXTERIOR/T8-R-EX-002.jpg",
+      "/assets/T8/EXTERIOR/T8-R-EX-003.jpg",
+      "/assets/T8/EXTERIOR/T8-R-EX-004.jpg",
+      "/assets/T8/INTERIOR/T8-R-IN-001.jpg",
+      "/assets/T8/INTERIOR/T8-R-IN-002.jpg",
+      "/assets/T8/INTERIOR/T8-R-IN-003.jpg",
+      "/assets/T8/INTERIOR/T8-R-IN-004.jpg",
     ];
     const imagePromises = images.map((src) => {
       return new Promise((resolve, reject) => {
@@ -81,7 +113,7 @@ const App = () => {
   }
   return (
     <main className="gap-[50px] overflow-x-hidden overflow-y-hidden bg-neutral-100 dark:bg-[#010103]">
-      <div className="max-w-9xl mx-auto">
+      <div className="max-w-9xl bg-future mx-auto bg-cover bg-center bg-no-repeat dark:bg-[#010103] dark:bg-none">
         <Hero />
       </div>
       <Footer />
